7267524a0b00c37799a79bc8f11a208e01a2281f
lvyitian <535481388@qq.com>
2020-05-24 11:33:17 +0800

    b

--- brainfuck/src/top/dsbbs2/brainfuck/Main.java
+++ brainfuck/src/top/dsbbs2/brainfuck/Main.java
@@ -42,67 +42,74 @@ public class Main
     }
   }
 
+  public static long countString(final String o, final String target)
+  {
+    long count = 0;
+    int lastIndex = o.indexOf(target);
+    while (lastIndex != -1) {
+      count++;
+      lastIndex = o.indexOf(target, lastIndex + 1);
+    }
+    return count;
+  }
+
   public static void eval(final String source, final Vector<Character> ca, final Reference<AtomicInteger> index,
       final Scanner s)
   {
+    if (Main.countString(source, "[") != Main.countString(source, "]")) {
+      throw new RuntimeException("the number of [ doesn't match with the number of ]");
+    }
     Main.extendArray(ca, index);
     final char[] arr = source.toCharArray();
-    int whileloc = -1;
     for (int c = 0; c < arr.length; c++) {
       final char i = arr[c];
       if (Objects.equals(i, ' ') || Objects.equals(i, '\n') || Objects.equals(i, '\r')) {
         continue;
       }
-      if (whileloc == -1) {
-        if (Objects.equals(i, '[')) {
-          whileloc = c;
-          continue;
-        }
-        if (Objects.equals(i, '>')) {
-          index.value.incrementAndGet();
-          Main.extendArray(ca, index);
-          continue;
-        }
-        if (Objects.equals(i, '<')) {
-          index.value.decrementAndGet();
-          continue;
-        }
-        if (Objects.equals(i, '+')) {
-          final char ch = ca.get(index.value.get());
-          ca.remove(index.value.get());
-          ca.add(index.value.get(), (char) (ch + 1));
-          continue;
-        }
-        if (Objects.equals(i, '-')) {
-          final char ch = ca.get(index.value.get());
-          ca.remove(index.value.get());
-          ca.add(index.value.get(), (char) (ch - 1));
-          continue;
-        }
-        if (Objects.equals(i, '.')) {
-          System.out.print(ca.get(index.value.get()));
-          continue;
-        }
-        if (Objects.equals(i, ',')) {
-          ca.remove(index.value.get());
-          try {
-            ca.add(index.value.get(), s.next().toCharArray()[0]);
-          } catch (final Throwable e) {
-            throw new RuntimeException(e);
-          }
-          continue;
-        }
-        throw new RuntimeException("bad token at " + (c + 1));
-      } else if (Objects.equals(i, ']')) {
-        final String ws = Main.charArrToString(arr, whileloc + 1, c);
+      if (Objects.equals(i, '[')) {
+        final int tempi = source.lastIndexOf(']');
+        final String ws = Main.charArrToString(arr, c + 1, tempi);
         while (ca.get(index.value.get()) != '\0') {
           Main.eval(ws, ca, index, s);
         }
-        whileloc = -1;
-        continue;
-      } else {
+        c = tempi;
         continue;
       }
+      if (Objects.equals(i, '>')) {
+        index.value.incrementAndGet();
+        Main.extendArray(ca, index);
+        continue;
+      }
+      if (Objects.equals(i, '<')) {
+        index.value.decrementAndGet();
+        continue;
+      }
+      if (Objects.equals(i, '+')) {
+        final char ch = ca.get(index.value.get());
+        ca.remove(index.value.get());
+        ca.add(index.value.get(), (char) (ch + 1));
+        continue;
+      }
+      if (Objects.equals(i, '-')) {
+        final char ch = ca.get(index.value.get());
+        ca.remove(index.value.get());
+        ca.add(index.value.get(), (char) (ch - 1));
+        continue;
+      }
+      if (Objects.equals(i, '.')) {
+        System.out.print(ca.get(index.value.get()));
+        continue;
+      }
+      if (Objects.equals(i, ',')) {
+        ca.remove(index.value.get());
+        try {
+          ca.add(index.value.get(), s.next().toCharArray()[0]);
+        } catch (final Throwable e) {
+          throw new RuntimeException(e);
+        }
+        continue;
+      }
+      throw new RuntimeException("bad token at " + (c + 1));
     }
   }
 }
